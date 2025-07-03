6949698ba3b165ae3aef27da67b7f3f80d7460c4
Galang Swastika Ramadhan <swastikaramadhan05@gmail.com>
2025-07-03 21:42:51 +0700

    add fe via upload

---
+++ NusaPay-FE/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+  domains: ['lh3.googleusercontent.com'],
+},
+productionSourceMaps: true, // kalau mau aktif juga di production
+};
+
+export default nextConfig;
---
+++ NusaPay-FE/src/types/recipient.ts
@@ -0,0 +1,44 @@
+//data penerima
+
+// Keperluan UI (gaada companyId dll untuk ga ditampilin)
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export interface Recipient{
+    _id: string
+    name: string
+    bankCode: string;
+    bankAccount: string //no rekening
+    bankAccountName: string //nama bank
+    amountTransfer: number// jumlah tf pengirim
+    currency: string //wallet si pengirim 
+    localCurrency: string //wallet penerima
+}
+
+  
+
+// Tipe asli dari data backend 
+export interface Employee{
+    id: string
+    companyId: string
+    companyName : string
+    name: string
+    bankCode: string;
+    bankAccount: string //no rekening
+    bankAccountName: string //nama bank
+    walletAddress : string
+    networkChainId : number
+    amountTransfer: number// jumlah tf pengirim
+    currency: string //wallet si pengirim 
+    localCurrency: string //wallet penerima
+}
+
+export interface LoadPayloadWithCompanyId {
+    companyId: string;
+}  
+
+export interface LoadPayloadForGroupId {
+    groupId: string;
+}
+
+
+
+
